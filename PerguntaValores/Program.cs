698b640cbcaad8371f361e07c14481277876a05f
Daniel <danielmordelfer@gmail.com>
2023-03-10 13:29:22 +0000

    first try volume and area

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(args[0]);
+            int r = Convert.ToInt32(args[1]);
+            double pi = Math.PI;
+            double V = pi * r ^ 2 * a;
+            double Sa = 2 * pi * r * (r + a);
+
+            Console.WriteLine($"Volume = " + V);
+            Console.WriteLine($"Area = " + Sa);
+        }
+    }
+}
