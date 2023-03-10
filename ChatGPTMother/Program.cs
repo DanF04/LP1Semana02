61f5655a17edbf6e4fc90e3fd35a25a710303d67
Daniel <danielmordelfer@gmail.com>
2023-03-10 18:44:30 +0000

    Two qustions first try

---
+++ ChatGPTMother/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ChatGPTMother
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string question, response;
+
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            switch (question)
+            {
+                case "Hello?":
+                    response = "hi.....inferior human.";
+                    continue;
+                case "Can i pass?":
+                    response = "YOU SHALL NOT PASS!!!!!!!!!!!!!!";
+                    continue;
+
+                case "EXIT":
+                    response = "Quitting........not because you decided, it's because i can stand such a dumb creator asking me questions.";
+                    break;
+            }
+
+            Console.WriteLine(response);
+        }
+    }
+}
