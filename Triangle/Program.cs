b132be04cd1793734d6171b6fe2d622c24b5a272
Daniel <danielmordelfer@gmail.com>
2023-03-10 19:53:24 +0000

    triangle creation through args

---
+++ Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int TriSize = Convert.ToInt32(args[0]);
+
+            string x = args[1];
+
+            for (int i = 0; i < TriSize; i++)
+            { 
+               x += args[1];
+               Console.WriteLine(x);
+            }
+        }
+    }
+}
