c750fa567c7fb91b4804ef4949ae9530a484b93d
Daniel <danielmordelfer@gmail.com>
2023-03-10 13:29:56 +0000

    fix volume

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -9,7 +9,7 @@ namespace PerguntaValores
             int a = Convert.ToInt32(args[0]);
             int r = Convert.ToInt32(args[1]);
             double pi = Math.PI;
-            double V = pi * r ^ 2 * a;
+            double V = pi * r * r * a;
             double Sa = 2 * pi * r * (r + a);
 
             Console.WriteLine($"Volume = " + V);
