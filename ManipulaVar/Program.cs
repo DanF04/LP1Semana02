5f07494b61d611322e00b85aa270b1e2bb4e438d
Daniel <danielmordelfer@gmail.com>
2023-03-10 10:41:23 +0000

    Using prefix in arg

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            int x = Convert.ToInt32(args[0]);
+            int xx = x;
+            xx++;
+            
+
+            Console.WriteLine(xx);
+        }
+    }
+}
