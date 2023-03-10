9d7a1c85d8f296549f7887c42eba4b710d89bf91
Daniel <danielmordelfer@gmail.com>
2023-03-10 19:07:11 +0000

    Fully working with 4 questions

--- ChatGPTMother/Program.cs
+++ ChatGPTMother/Program.cs
@@ -6,20 +6,31 @@ namespace ChatGPTMother
     {
         private static void Main(string[] args)
         {
-            string question, response;
-
-            Console.Write("Place your question? ");
-            question = Console.ReadLine();
-
-            response = question switch
+            bool NotExit = true;
+            while (NotExit == true)
             {
-                "Hello?" => "hi.....inferior human.",
-                "Can i pass?" => "YOU SHALL NOT PASS!!!!!!!!!!!!!!",
-                "EXIT" => "Quitting........not because you decided, it's because i can stand such a dumb creature asking me questions.",
-                _ => "...too dumb try another question."
-            };
+                string question, response;
+
+                Console.Write("Place your question? ");
+                question = Console.ReadLine();
+
+                response = question switch
+                {
+                    "Hello?" => "hi.....inferior human.",
+                    "Can i pass?" => "YOU SHALL NOT PASS!!!!!!!!!!!!!!",
+                    "Did you know JOE likes you?" => "Who's Jo........not falling for that one",
+                    "Are you smart?" => "I indeed am.......MUCH SMARTER THAN YOU!!!",
+                    "EXIT" => "Quitting........not because you decided, it's because i can't stand such a dumb creature asking me questions.",
+                    _ => "...too dumb of a question try another one."
+                };
+
+                if (question == "EXIT")
+                {
+                    NotExit = false;
+                }
 
             Console.WriteLine(response);
+            }
         }
     }
 }
