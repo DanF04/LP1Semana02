e5a8ca62c1250223a5ca6730b1d0dbae5538702f
Daniel <danielmordelfer@gmail.com>
2025-02-28 12:40:54 +0000

    Program that prints all types max and min values

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            //Variables for all types max and mi values
+            sbyte sb = sbyte.MaxValue;
+            sbyte sb2 = sbyte.MinValue;
+            short sh = short.MaxValue;
+            short sh2 = sbyte.MinValue;
+            int nt = int.MaxValue;
+            int nt2 = int.MinValue;
+            long lo = long.MaxValue;
+            long lo2 = long.MinValue;
+            byte by = byte.MaxValue;
+            byte by2 = byte.MinValue;
+            ushort us = ushort.MaxValue;
+            ushort us2 = ushort.MinValue;
+            uint ui = uint.MaxValue;
+            uint ui2 = uint.MinValue;
+            ulong ul = ulong.MaxValue;
+            ulong ul2 = ulong.MinValue;
+            char ch = char.MaxValue;
+            char ch2 = char.MinValue;
+            decimal de = decimal.MaxValue;
+            decimal de2 = decimal.MinValue;
+
+            //Printing the respective values
+            Console.WriteLine(sb);
+            Console.WriteLine(sb2);
+            Console.WriteLine(sh);
+            Console.WriteLine(sh2);
+            Console.WriteLine(nt);
+            Console.WriteLine(nt2);
+            Console.WriteLine(lo);
+            Console.WriteLine(lo2);
+            Console.WriteLine(by);
+            Console.WriteLine(by2);
+            Console.WriteLine(us);
+            Console.WriteLine(us2);
+            Console.WriteLine(ui);
+            Console.WriteLine(ui2);
+            Console.WriteLine(ul);
+            Console.WriteLine(ul2);
+            Console.WriteLine(ch);
+            Console.WriteLine(ch2);
+            Console.WriteLine(de);
+            Console.WriteLine(de2);
+
+        }
+    }
+}
