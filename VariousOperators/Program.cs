64f8bde45d02896ca2979086d0e8a1925bb637c1
Daniel <danielmordelfer@gmail.com>
2025-02-28 09:54:10 +0000

    New program that ask for input and gives that input altered by different operators

--- VariousOperators/Program.cs
+++ VariousOperators/Program.cs
@@ -6,7 +6,22 @@ namespace VariousOperators
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+
+            //Aks for the user input
+            Console.WriteLine("Insere um número-não negativo:");
+            string intn = Console.ReadLine();
+
+            //Converts user input into an sbyte
+            byte n = byte.Parse(intn);
+
+            //Divides the variable by 2
+            Console.WriteLine($"{n/2}");
+            //Shifts the variable by 3
+            Console.WriteLine($"{n<<3}");
+            //XOR by 6
+            Console.WriteLine($"{n^6}");
+            //Gives true if the variable is bigger than 10
+            Console.WriteLine($"{n>10}");
         }
     }
 }
