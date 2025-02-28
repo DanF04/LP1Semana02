ddadfac2986666256360d52cad8b748d8b01df33
Daniel <danielmordelfer@gmail.com>
2025-02-28 09:28:30 +0000

    Ask for input and tranform into sbyte

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            //Asks for user input
+            Console.WriteLine("Insere um número inteiro:");
+            string smoo = Console.ReadLine();
+            
+            //Converts user input into an sbyte
+            sbyte convert = sbyte.Parse(smoo);
+
+            Console.WriteLine ($"{++convert}");
+            Console.WriteLine ($"{--convert}");
+
+        }
+    }
+}
