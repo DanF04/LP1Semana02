3e89fd03855c685c1f42b127f725e9bb0f41061b
Daniel <danielmordelfer@gmail.com>
2025-02-28 09:35:20 +0000

    new coments

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -13,8 +13,10 @@ namespace SmoothOperator
             
             //Converts user input into an sbyte
             sbyte n = sbyte.Parse(smoo);
-
+            
+            //Removed 1 from the variable
             Console.WriteLine ($"{--n}");
+            //Added 1 to the variable
             Console.WriteLine ($"{++n}");
 
         }
