b045493832adedf4a568a39754145d16e5768746
Daniel <danielmordelfer@gmail.com>
2025-02-28 11:52:53 +0000

    New program that asks for the user to input and height and radius

---
+++ Cilindro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere a altura do cilindro");
+            string altura = Console.ReadLine();
+            Console.WriteLine("Insere o raio do cilindro");
+            string raio = Console.ReadLine();
+
+            double a = double.Parse(altura);
+            double r = double.Parse(raio);
+
+            double V = Math.PI * Math.Pow(r,2) * a;
+            double Sa = 2 * Math.PI * r * (r + a);
+
+            
+
+        }   
+    }
+}
